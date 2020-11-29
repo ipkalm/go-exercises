5c14496ba543c225dd2837b4d0fd5ed5d15e704d
ipkalm <ipkalm@outlook.com>
2020-11-29 16:06:48 +0700

    exercise 2-6 done

--- ninja-level-2/exercise-5/main.go
+++ ninja-level-2/exercise-5/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	x := `hell
-	yeah, Im a h4ck3rman, using C on $t3r0ids`
+	yeah, Im a h4ck3rman, using C on "$t3r0ids"`
 
 	fmt.Println(x)
 }
---
+++ ninja-level-2/exercise-6/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const (
+	year21 = 2021 + iota
+	year22
+	year23
+	year24
+)
+
+func main() {
+	fmt.Println(year21, year22, year23, year24)
+}
