d7206a6395751734c6da1457ef739b89ff908daf
ipkalm <ipkalm@outlook.com>
2020-11-29 09:19:22 +0700

    exercise 2 complete

---
+++ ninja-level-1/exercise-2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	//zero values
+	// for int = 0
+	// -//- string = ""
+	// -//- bool = false
+}
