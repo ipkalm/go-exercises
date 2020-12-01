9933495628792c3c45b902b917ac675c4c66dc1f
ipkalm <ipkalm@outlook.com>
2020-12-02 00:03:04 +0700

    exercise 3-9 done

---
+++ ninja-level-3/exercise-9/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	favSport := "dropshot"
+	switch favSport {
+	case "dropshot":
+		fmt.Println(favSport)
+	case "rumble":
+		fmt.Println(favSport)
+	case "hoops":
+		fmt.Println(favSport)
+	}
+}
