52f58cadf9f27d0dfa4d9bc34ce3691b2dc9a1ff
ipkalm <ipkalm@outlook.com>
2020-12-17 03:15:11 +0700

    remove data for ninja-level-012 exercise №2

--- ninja-level-012/exercise-01/dog/dog.go
+++
@@ -1,9 +0,0 @@
-// Package dog provide func's which
-// convert human and dog years
-package dog
-
-// Years takes human years and
-// turns them into dog years 1:7
-func Years(hy int) int {
-	return hy / 7
-}
