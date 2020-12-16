b5fd3c22287811694e650fc99a7492bf8c4b0af8
ipkalm <ipkalm@outlook.com>
2020-12-17 02:59:40 +0700

    exercise 12-01 is done

---
+++ ninja-level-012/exercise-01/dog/dog.go
@@ -0,0 +1,9 @@
+// Package dog provide func's which
+// convert human and dog years
+package dog
+
+// Years takes human years and
+// turns them into dog years 1:7
+func Years(hy int) int {
+	return hy / 7
+}
